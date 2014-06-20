74a943e64509b402162c8c6566500291b6779a7e
Salvador Gironès <salvadorgirones@gmail.com>
2014-06-20 15:55:07 +0200

    foo

--- factory.go
+++ factory.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-  "github.com/fsouza/go-dockerclient"
-  "os/exec"
-  "log"
-  "strings"
-  "fmt"
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/dotcloud/docker/archive"
+	"github.com/fsouza/go-dockerclient"
 )
 
 type Queue []*Image
@@ -13,45 +16,69 @@ type Queue []*Image
 type CompletedBuilds []*Image
 
 type Factory struct {
-  cache bool
-  docker *docker.Client
+	Cache  bool
+	Docker *docker.Client
 }
 
 func NewFactory(endpoint string) (*Factory, error) {
-  _, err := docker.NewClient(endpoint)
-  if err != nil {
-    return nil, err
-  }
+	e, err := docker.NewClient(endpoint)
+	if err != nil {
+		return nil, err
+	}
 
-  return &Factory{
-    cache: false,
-    docker: nil,
-  }, nil
+	return &Factory{
+		Cache:  false,
+		Docker: e,
+	}, nil
 }
 
-func (d *Factory) Build(img *Image) bool {
-  log.Println("Executing the before command...")
-  if err := execute(img.Build.Before); err != nil {
-    log.Fatal(err)
-  }
-  log.Println("Done")
+func (f *Factory) Build(img *Image) bool {
+	log.Println("Executing the before command...")
+	if err := execute(img.Build.Before); err != nil {
+		log.Fatal(err)
+		return false
+	}
+	log.Println("Done")
 
-  // docker build
+	log.Println("Preparing the context...")
+	r, err := archive.Tar(img.Path, archive.Uncompressed)
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
 
-  log.Println("Executing the after command...")
-  if err := execute(img.Build.After); err != nil {
-    log.Fatal(err)
-  }
-  log.Println("Done")
+	out := bytes.NewBufferString("")
+	opts := docker.BuildImageOptions{
+		Name:           img.Tag,
+		NoCache:        !f.Cache,
+		RmTmpContainer: true,
+		InputStream:    r,
+		OutputStream:   out,
+	}
 
-  return true
+	log.Println("Building image...")
+	if err := f.Docker.BuildImage(opts); err != nil {
+		log.Fatal(err)
+		return false
+	}
+	log.Println(out)
+	log.Println("Done")
+
+	log.Println("Executing the after command...")
+	if err := execute(img.Build.After); err != nil {
+		log.Fatal(err)
+		return false
+	}
+	log.Println("Done")
+
+	return true
 }
 
 func execute(cmd string) error {
-  cmd_params := strings.Split(cmd, " ")
-  if output, err := exec.Command(cmd_params[0], cmd_params[1:]...).CombinedOutput(); err != nil {
-    return fmt.Errorf("Error trying to use git: %s (%s)", err, output)
-  }
+	cmd_params := strings.Split(cmd, " ")
+	if output, err := exec.Command(cmd_params[0], cmd_params[1:]...).CombinedOutput(); err != nil {
+		return fmt.Errorf("Error trying to use git: %s (%s)", err, output)
+	}
 
-  return nil
+	return nil
 }
--- image.go
+++ image.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/libgit2/git2go"
-	"gopkg.in/yaml.v1"
 	"strings"
+
+	"gopkg.in/yaml.v1"
 )
 
 type Image struct {
 	Tag      string
 	Uri      string
 	Context  string
+	Path     string
 	Build    ImageBuildConfig
 	Children []*Image // Images that need to be build before this
 }
@@ -18,8 +19,6 @@ type Image struct {
 type ImageBuildConfig struct {
 	Before string
 	After  string
-	Cache  bool
-	Path   string
 }
 
 // data => YAML content
@@ -34,10 +33,6 @@ func ImageList(data []byte) ([]*Image, error) {
 func (image *Image) Checkout() (string, error) {
 	elements := strings.Split(image.Uri, "/")
 	name := elements[len(elements)-1]
-	options := &git.CloneOptions{
-		CheckoutBranch: "master",
-	}
 	fmt.Printf("Checking out %s: %s...\n", name, image.Uri)
-	git.Clone(image.Uri, "/tmp/"+name, options)
 	return "", nil
 }
